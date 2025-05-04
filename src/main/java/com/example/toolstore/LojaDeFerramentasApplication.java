e3f30198dda4ed75b456adf1701967b3d2ee7273
Lucas Moreira <lukanbr11@gmail.com>
2025-05-03 22:22:45 -0300

    initial config

---
+++ src/main/java/com/example/toolstore/LojaDeFerramentasApplication.java
@@ -0,0 +1,13 @@
+package com.example.toolstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaDeFerramentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaDeFerramentasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/toolstore/LojaDeFerramentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toolstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaDeFerramentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
