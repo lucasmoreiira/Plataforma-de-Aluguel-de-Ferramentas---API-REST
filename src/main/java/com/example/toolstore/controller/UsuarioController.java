1f5bfbdb6eec3be754060b2b57c5c404d8e5084b
Lucas Moreira <lukanbr11@gmail.com>
2025-05-03 22:49:57 -0300

    feat: add Usuario Controller with Get and Post Methods

---
+++ src/main/java/com/example/toolstore/controller/UsuarioController.java
@@ -0,0 +1,33 @@
+package com.example.toolstore.controller;
+
+import com.example.toolstore.model.Usuario;
+import com.example.toolstore.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping
+    public List<Usuario> getAllUsuarios(){
+        return usuarioService.getAllUsuarios();
+    }
+
+    @GetMapping("/{id}")
+    public Usuario getUsuarioById(@PathVariable String id){
+        return usuarioService.getUsuarioById(id);
+    }
+
+    @PostMapping
+    public Usuario createUsuario(@RequestBody Usuario usuario){
+        return usuarioService.saveUsuario(usuario);
+    }
+
+
+}
