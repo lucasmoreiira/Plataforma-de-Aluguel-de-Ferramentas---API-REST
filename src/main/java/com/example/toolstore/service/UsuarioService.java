4110daee7fdf0aef75115d849ac047f27ce7b3a9
Lucas Moreira <lukanbr11@gmail.com>
2025-05-03 22:49:28 -0300

    feat: add Usuario Service

---
+++ src/main/java/com/example/toolstore/service/UsuarioService.java
@@ -0,0 +1,31 @@
+package com.example.toolstore.service;
+
+import com.example.toolstore.model.Usuario;
+import com.example.toolstore.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<Usuario> getAllUsuarios(){
+        return usuarioRepository.findAll();
+    }
+
+    public Usuario getUsuarioById(String id){
+        return usuarioRepository.findById(id).orElse(null);
+    }
+
+    public Usuario saveUsuario(Usuario usuario){
+        return usuarioRepository.save(usuario);
+    }
+    public void deleteUsuarioBy(String id){
+        usuarioRepository.deleteById(id);
+    }
+
+}
